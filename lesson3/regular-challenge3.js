0a4721b86e0c88abb27d1b409f75f43a9f86c2cf
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2023-01-25 21:36:40 -0800

    updates

--- lesson3/regular-challenge3.js
+++ lesson3/regular-challenge3.js
@@ -2,13 +2,13 @@
 //Use a for loop to console log all of the elements plus the index and a comma
 
 function animalList(){
-    //array of 5 animals
-   const animal = ["cats", "dogs", "lions", "birds", "bear"];
-   console.log("Index" + "  " + "Name");
-   
-   //loop to console log all of the elements plus the index and a comma
-   for (var i=0;i < animal.length;i++) {
-    console.log(i + "     " + animal[i] + ", ");
-  }
+  //array of 5 animals
+ const animal = ['',"cats", "dogs", "lions", "birds", "bear", "frog"];
+ console.log("Index" + "  " + "Name");
+ 
+ //loop to console log all of the elements plus the index and a comma
+ for (var i =1;i < animal.length;i++) {
+  console.log(i  + ", " + animal[i]);
+}
 }
 animalList();
