b3d31126b26ef11a9ec1de217cec8078cd07ee58
ABEL MENGSTU <94520834+abeldbase@users.noreply.github.com>
2023-01-25 21:12:32 -0800

    adding my assignemnet Regular Challenge 3 file

---
+++ lesson3/regular-challenge3.js
@@ -0,0 +1,14 @@
+// Create an array that has at least 5 strings:
+//Use a for loop to console log all of the elements plus the index and a comma
+
+function animalList(){
+    //array of 5 animals
+   const animal = ["cats", "dogs", "lions", "birds", "bear"];
+   console.log("Index" + "  " + "Name");
+   
+   //loop to console log all of the elements plus the index and a comma
+   for (var i=0;i < animal.length;i++) {
+    console.log(i + "     " + animal[i] + ", ");
+  }
+}
+animalList();
